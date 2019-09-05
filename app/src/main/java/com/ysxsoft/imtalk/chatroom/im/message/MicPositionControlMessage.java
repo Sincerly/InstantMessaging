d39d9c05d26d0af637e27218b7222f9594349b25
huguangcai <2694675654@qq.com>
2019-09-05 09:14:16 +0800

    修改

--- app/src/main/java/com/ysxsoft/imtalk/chatroom/im/message/MicPositionControlMessage.java
+++ app/src/main/java/com/ysxsoft/imtalk/chatroom/im/message/MicPositionControlMessage.java
@@ -1,7 +1,12 @@
 package com.ysxsoft.imtalk.chatroom.im.message;
 
 import android.os.Parcel;
+import android.util.Log;
 
+import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
 import com.ysxsoft.imtalk.chatroom.model.MicBehaviorType;
 import com.ysxsoft.imtalk.chatroom.model.MicPositionsBean;
 import com.ysxsoft.imtalk.chatroom.utils.log.SLog;
@@ -14,6 +19,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.rong.common.ParcelUtils;
 import io.rong.imlib.MessageTag;
 import io.rong.imlib.model.MessageContent;
 
@@ -32,24 +38,26 @@ public class MicPositionControlMessage extends MessageContent {
         String jsonStr = null;
         try {
             jsonStr = new String(data, "UTF-8");
+            Log.d("tag=接收",jsonStr);
         } catch (UnsupportedEncodingException e) {
             SLog.e(TAG, "UnsupportedEncodingException ", e);
         }
 
         try {
             JSONObject jsonObj = new JSONObject(jsonStr);
-
             setCmd(jsonObj.optInt("cmd"));
             setTargetUserId(jsonObj.optString("targetUserId"));
             setTargetPosition(jsonObj.optInt("targetPosition"));
-            JSONArray positions = jsonObj.optJSONArray("micPositions");
-            List<MicPositionsBean> infoList = new ArrayList<>();
-            if (positions != null && positions.length() > 0) {
-                for (int i = 0; i < positions.length(); i++) {
-                    infoList.add(MicPositionsBean.parseJsonToMicPositionInfo(positions.getJSONObject(i)));
+            if (jsonObj.has("micPositions")){
+                JSONArray positions = jsonObj.optJSONArray("micPositions");
+                List<MicPositionsBean> infoList = new ArrayList<>();
+                if (positions != null && positions.length() > 0) {
+                    for (int i = 0; i < positions.length(); i++) {
+                        infoList.add(MicPositionsBean.parseJsonToMicPositionInfo(positions.getJSONObject(i)));
+                    }
                 }
+                setMicPositions(infoList);
             }
-            setMicPositions(infoList);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -89,7 +97,27 @@ public class MicPositionControlMessage extends MessageContent {
 
     @Override
     public byte[] encode() {
-        return new byte[0];
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            jsonObj.put("cmd", cmd);
+            jsonObj.put("targetUserId", getTargetUserId());
+            jsonObj.put("targetPosition", getTargetPosition());
+            String json=JSON.toJSONString(getMicPositions());
+
+            com.alibaba.fastjson.JSONArray jsonArray = com.alibaba.fastjson.JSONArray.parseArray(json);
+            Log.d("tag=发送",jsonArray.toJSONString());
+            JSONArray array=new JSONArray(jsonArray.toJSONString());
+            jsonObj.put("micPositions", array);
+            byte[] bytes = jsonObj.toString().getBytes("UTF-8");
+            return bytes;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("tag=JSONObject=",jsonObj.toString());
+        return null;
     }
 
 
@@ -113,7 +141,7 @@ public class MicPositionControlMessage extends MessageContent {
         this.cmd = in.readInt();
         this.targetUserId = in.readString();
         this.targetPosition = in.readInt();
-        this.micPositions = in.createTypedArrayList(MicPositionsBean.CREATOR);
+        this.micPositions =in.createTypedArrayList(MicPositionsBean.CREATOR);
     }
 
     public static final Creator<MicPositionControlMessage> CREATOR = new Creator<MicPositionControlMessage>() {
